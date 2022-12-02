c025097c81c46e8ac09ab9abb9c4dc5627abdce0
RyanLei00 <98862212+RyanLei00@users.noreply.github.com>
2022-12-02 17:01:02 -0500

    Update script.js

--- Level2JS_Classwork/script.js
+++ Level2JS_Classwork/script.js
@@ -25,3 +25,32 @@ function changeWidth(w){
     textWidth = w;
     display();
 }
+
+function initializeCSS(){
+    textOutput = document.getElementById("textdiv");
+    fontSizeOutput = document.getElementById("sizeout");
+    
+    fontSize = 24;
+}
+
+function modifyFontSize(mod){
+    fontSize += mod;
+    
+    if(fontSize < 4)
+        fontSize = 4;
+    
+    displayCSS();
+}
+
+function displayCSS(){
+    fontSizeOutput.innerHTML = fontSize;
+    textOutput.style.fontSize = fontSize + "pt";
+}
+
+function getRandomInteger(lower, upper){
+    //R = parseInt(rnd * (upper - (lower - 1)) + lower
+    var multiplier = upper - (lower - 1);
+    var rnd = parseInt(Math.random() * multiplier) + lower;
+    
+    return rnd;
+}
